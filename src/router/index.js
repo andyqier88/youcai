3ac29575809602d5fb7343aaad0bffc7ab3cdd19
andyqier88 <331875318@qq.com>
2017-10-14 17:39:19 +0800

    新增人才平台路由

--- src/router/index.js
+++ src/router/index.js
@@ -2,16 +2,17 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Header from '@/components/Header'
 import Index from '@/pages/Index'
+import TalentPlatfor from '@/pages/talent_platfor'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    // {
-    //   path: '/talent_platgor',
-    //   name: 'Hello',
-    //   component: Header
-    // },
+    {
+      path: '/talent_platfor',
+      name: 'TalentPlatfor',
+      component: TalentPlatfor
+    },
     {
       path:'/',
       name:'index',
