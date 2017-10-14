35b439f558fe37aba5e7b3262048eab23f4a92bf
andyqier88 <331875318@qq.com>
2017-10-14 17:41:07 +0800

    修改内容

--- src/pages/talent_platfor.vue
+++ src/pages/talent_platfor.vue
@@ -1,3 +1,20 @@
 <template>
-  
+  <div>
+    <top-header></top-header>   
+  </div>
 </template>
+
+<script>
+  import topHeader from '@/components/Header'
+  import footerbar from '@/components/Footer'
+  export default{
+    components:{
+      topHeader,
+      footerbar
+    }
+  }
+</script>
+
+<style>
+  
+</style>
